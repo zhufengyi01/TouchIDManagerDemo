79929d260bfc80547b0b574b8c0f0b9e2ddf8b36
朱封毅 <673229963@qq.com>
2016-04-14 10:17:28 +0800

    first commit

---
+++ TouchIDManager.h
@@ -0,0 +1,13 @@
+//
+//  TouchIDManager.h
+//  TouchIDManagerDemo
+//
+//  Created by 朱封毅 on 14/04/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TouchIDManager : NSObject
+
+@end
---
+++ TouchIDManager.m
@@ -0,0 +1,13 @@
+//
+//  TouchIDManager.m
+//  TouchIDManagerDemo
+//
+//  Created by 朱封毅 on 14/04/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import "TouchIDManager.h"
+
+@implementation TouchIDManager
+
+@end
